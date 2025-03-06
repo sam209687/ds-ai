ff0737d3f4b11cdeedaf23421300b1cecc099b3f
sam209687 <sam968766@gmail.com>
2025-03-06 13:55:18 +0530

    correction 2

--- auth.ts
+++ auth.ts
@@ -1,15 +1,41 @@
 import NextAuth from "next-auth"
 // import { PrismaAdapter } from "@auth/prisma-adapter"
-// import { prisma } from "@/lib/db"
-import authConfig from "@/auth.config"
+import { prisma } from "@/lib/db"
+import authConfig from "./auth.config"
+import Credentials from "next-auth/providers/credentials"
+import { compare } from "bcryptjs"
 
-export const {
-  handlers: { GET, POST },
-  auth,
-  signIn,
-  signOut,
-} = NextAuth({
+export const { handlers, auth, signIn, signOut } = NextAuth({
   // adapter: PrismaAdapter(prisma),
   session: { strategy: "jwt" },
   ...authConfig,
+  providers: [
+    ...authConfig.providers,
+    Credentials({
+      async authorize(credentials) {
+        const { email, password } = credentials as {
+          email: string
+          password: string
+        }
+
+        const user = await prisma.user.findUnique({
+          where: { email },
+        })
+
+        if (!user?.password) return null
+
+        const isValid = await compare(password, user.password)
+
+        if (!isValid) return null
+
+        return {
+          id: user.id,
+          email: user.email,
+          name: user.name,
+          image: user.image,
+          role: user.role,
+        }
+      },
+    }),
+  ],
 })
